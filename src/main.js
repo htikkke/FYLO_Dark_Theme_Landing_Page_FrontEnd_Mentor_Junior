808bd7e8838f88574c7cb9409fb51d6e673f6085
htikkke <htikew574@gmail.com>
2026-07-03 21:17:55 +0630

    add js for error-active state.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const inputEmail = document.getElementById("email-input");
+const submitButton = document.getElementById("submit-button");
+const errorAlert = document.getElementById("error-alert");
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+submitButton.addEventListener("click", () => {
+  if (!emailPattern.test(inputEmail.value)) {
+    errorAlert.classList.remove("hidden");
+    setTimeout(() => {
+      errorAlert.classList.add("hidden");
+      inputEmail.value = "";
+    }, 3000);
+  } else {
+    inputEmail.classList.add("border-5", "border-green-600");
+    setTimeout(() => {
+      inputEmail.classList.remove("border-5", "border-green-600");
+      inputEmail.value = "";
+    }, 3000);
+  }
+});
