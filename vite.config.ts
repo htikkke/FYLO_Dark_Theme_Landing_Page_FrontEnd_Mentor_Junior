6da227934a12c49a5b822f0700ba6d88cdba645a
htikkke <htikew574@gmail.com>
2026-07-06 11:18:44 +0630

    fix style paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/FYLO_Dark_Theme_Landing_Page_FrontEnd_Mentor_Junior/",
+  base: "./",
 });
